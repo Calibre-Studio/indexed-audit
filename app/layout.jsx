924c78d4f98cd103832af90eb88eec66ed96c1e4
Visionaire Labs <36512615+VisionaireLabs@users.noreply.github.com>
2026-07-18 22:37:39 +0200

    Add analytics: LinkedIn Insight, GA4, Meta Pixel to root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -79,6 +79,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} ${fragmentMono.variable}`}>
       <body>
+        {/* Analytics — LinkedIn Insight 7100092, GA4 G-Z190SM5JHP, Meta Pixel 1590155688538388 */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `_linkedin_partner_id="7100092";window._linkedin_data_partner_ids=window._linkedin_data_partner_ids||[];window._linkedin_data_partner_ids.push(_linkedin_partner_id);(function(l){if(!l){window.lintrk=function(a,b){window.lintrk.q.push([a,b])};window.lintrk.q=[]}var s=document.getElementsByTagName("script")[0];var b=document.createElement("script");b.type="text/javascript";b.async=true;b.src="https://snap.licdn.com/li.lms-analytics/insight.min.js";s.parentNode.insertBefore(b,s);})(window.lintrk);`,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-Z190SM5JHP');(function(){var g=document.createElement('script');g.async=true;g.src='https://www.googletagmanager.com/gtag/js?id=G-Z190SM5JHP';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(g,s);})();`,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1590155688538388');fbq('track','PageView');`,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(SCHEMA) }}
